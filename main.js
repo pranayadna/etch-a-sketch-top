55f91f383cde4214dc6698982ad651656419f1e5
pranayadna <pranayadnaap96@gmail.com>
2024-02-13 12:35:27 +0700

    Add button with onclick function

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
-const grids = [];
+let grids = [];
 
 const container = document.querySelector('.container');
 
-// Create n x n grid
 const createGrid = (n) => {
     if (n > 100) {
         return alert('Max input is 100');
-    }
+    } 
 
-    for (let i = 0; i < n ** 2; i++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.setAttribute('class', 'grid');
-        container.appendChild(gridDiv);
-        
-        grids.push(gridDiv);
-        
+    console.log(grids);
+    if(grids.length > 0){
+        grids.forEach(grid => {
+            container.removeChild(grid)
+        })
+        grids = [];
     }
     
-    const grid = document.querySelector('.grid');
+    for (let i = 0; i < n ** 2; i++) {
+        const gridDiv = document.createElement('div');
+        container.appendChild(gridDiv);
+        grids.push(gridDiv);   
+    }
+
     grids.forEach(grid => {
         grid.style.width = 400 / n + 'px';
         grid.style.height = 400 / n + 'px';
         grid.style.border = '1px solid black';
-    
+        
         grid.addEventListener('mouseover', () => {
             grid.style.backgroundColor = '#000'
         })
         
         grid.addEventListener('mouseout', () => {
             grid.style.backgroundColor = ''
-        })
-    })
+        }) 
+    }) 
 }
-
-createGrid(100)
-
-
-
