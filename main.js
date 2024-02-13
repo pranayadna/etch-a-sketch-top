d7f102933dd07c11bd035e7128e00144f5ef07df
pranayadna <pranayadnaap96@gmail.com>
2024-02-13 10:15:02 +0700

    Add create n grids function

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
-// 16x16 grid of square divs
-
 const grids = [];
 
 const container = document.querySelector('.container');
 
-// Create multiple element
-for (let i = 0; i < 256; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.setAttribute('class', 'grid');
-    container.appendChild(gridDiv);
+// Create n x n grid
+const createGrid = (n) => {
+    if (n > 100) {
+        return alert('Max input is 100');
+    }
+
+    for (let i = 0; i < n ** 2; i++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.setAttribute('class', 'grid');
+        container.appendChild(gridDiv);
+        
+        grids.push(gridDiv);
+        
+    }
     
-    grids.push(gridDiv);
+    const grid = document.querySelector('.grid');
+    grids.forEach(grid => {
+        grid.style.width = 400 / n + 'px';
+        grid.style.height = 400 / n + 'px';
+        grid.style.border = '1px solid black';
     
+        grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = '#000'
+        })
+        
+        grid.addEventListener('mouseout', () => {
+            grid.style.backgroundColor = ''
+        })
+    })
 }
 
-console.log(grids);
+createGrid(100)
 
-const grid = document.querySelector('.grid');
-grids.forEach(grid => {
-    
-    grid.style.width = '16px';
-    grid.style.height = '16px';
-    grid.style.border = '1px solid black';
-})
-// for (let i = 0; i < 5; i++) {
-// }
+
+
